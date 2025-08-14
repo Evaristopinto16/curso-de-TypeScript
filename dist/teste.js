294990ad8b3a9ba91030b1f90f941d36da2faa4d
Evaristopinto16 <evaristopinto16@gmail.com>
2025-08-14 19:22:43 +0300

    init

---
+++ dist/teste.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function somar(num1, num2) {
+    return num1 + num2;
+}
+somar(1, 2);
+//# sourceMappingURL=teste.js.map
